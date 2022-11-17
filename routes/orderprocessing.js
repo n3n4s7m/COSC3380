c977ab5db959ea6f3066ab3d3afecb6425b530b0
Group9Project <118316047+Group9Project@users.noreply.github.com>
2022-11-17 00:19:52 -0600

    Update orderprocessing.js

--- routes/orderprocessing.js
+++ routes/orderprocessing.js
@@ -5,7 +5,7 @@ var sql = require('mssql');
 
 /* GET insert product page. */
 router.get('/', function(req, res, next) {
-    var query = "SELECT * FROM [dbo].[orders] ORDER BY orderID;";
+    var query = "SELECT * FROM [dbo].[orders] ORDER BY orderID DESC;";
     sql.connect(config, function(err) {
         if(err) console.log(err);
         var request = new sql.Request();
